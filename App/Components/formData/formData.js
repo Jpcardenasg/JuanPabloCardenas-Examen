1c5be4a05dc9a9790607903098fbba4ae82bc54e
JuanPabloCardenasGonzalez <jpcardenasgonzalez@gmail.com>
2024-03-18 08:33:24 -0500

    feat: :sparkles: Estilos del form Data
    Estilos básicos para el form Data
    
    @CampusLands

--- App/Components/formData/formData.js
+++ App/Components/formData/formData.js
@@ -12,28 +12,35 @@ export class FormData extends HTMLElement {
             <form id="formData">
                 <fieldset>
                     <legend> Ingresa tu Nombre completo</legend>
-                <div class="input">
-                    <label for="description">Nombre:</label>
-                    <input type="text" id="name" name="name"
-                        placeholder="Ingresar nombre" required>
-                </div>
+                    <div class="input">
+                        <label for="description">Nombre:</label>
+                        <input type="text" id="name" name="name"
+                            placeholder="Ingresar nombre" required>
+                    </div>
+                </fieldset>
+
                 <fieldset>
                     <legend> Ingresa tu Numero de contacto</legend>
-                <div class="input">
-                    <label for="description">Numero:</label>
-                    <input type="number" id="contactNum" name="contactNum"
-                        placeholder="Ingresar numero" required>
-                </div>
+                    <div class="input">
+                        <label for="description">Numero:</label>
+                        <input type="number" id="contactNum" name="contactNum"
+                            placeholder="Ingresar numero" required>
+                    </div>
+                </fieldset>
+
                 <fieldset>
                     <legend> Ingresa tu correo</legend>
-                <div class="input">
-                    <label for="description">Email:</label>
-                    <input type="email" id="email" name="email"
-                        placeholder="Ingresar correo" required>
-                </div>
+                    <div class="input">
+                        <label for="description">Email:</label>
+                        <input type="email" id="email" name="email"
+                            placeholder="Ingresar correo" required>
+                    </div>
+                </fieldset>
+
                 <div class="containerSubmit">
                     <button id="data-button" type="submit">Enviar</button>
                 </div>
+
             </form>
         `;
 
